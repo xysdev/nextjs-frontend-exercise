9a3951faa39f97435053ae62ae9a7113cb8d4363
xysdev <geramian.erfan@outlook.com>
2026-03-28 17:33:51 +0100

    added a pre commit hook for tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,16 @@
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 import type { ViteUserConfig } from 'vitest/config';
 import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   test: {
     environment: 'happy-dom',
     globals: true,
