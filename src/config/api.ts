2756c5a89ca29e01c80a98b46f024210ebd5fa7e
xysdev <geramian.erfan@outlook.com>
2026-03-29 18:22:11 +0200

    use environment variable for vercel

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,1 @@
-const getBaseUrl = () => {
-  if (process.env.NEXT_PUBLIC_BASE_URL) return process.env.NEXT_PUBLIC_BASE_URL;
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-  return 'http://localhost:3000';
-};
-
-export const API_BASE_URL = getBaseUrl();
+export const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000';
